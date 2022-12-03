6776224885b2e766f05721c4447340f3fce7707a
cristian suarez <casm2203@gmail.com>
2022-12-02 21:53:08 -0500

    creando configuracion db

---
+++ server/database/db.js
@@ -0,0 +1,12 @@
+import { createPool } from "mysql2/promise";
+
+export const pool = createPool({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.USERDB,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+
+//docker run --name momentsSql -e MYSQL_ROOT_PASSWORD=colombiaprr -e MYSQL_DATEBASE=moments -p 3306:3306  -d mysql
